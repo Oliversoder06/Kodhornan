69ff0ef782117cb01172c307670d9583a39eddc9
Oliversoder06 <lexrudolf06@gmail.com>
2026-01-29 16:29:18 +0100

    Fix: Convert client from submodule to regular folder + update .gitignore

---
+++ client/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Kodhörnan",
+  description: "En plats för att lära sig kod.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="sv">
+      <body>{children}</body>
+    </html>
+  );
+}
